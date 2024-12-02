c1e8e87c73e117a2851c1012c05b754f9651d1d9
AlenaZin <nomak1493@gmail.com>
2024-12-02 11:48:31 +0300

    add design for web_2

--- src/components/NewsList/NewsList.tsx
+++ src/components/NewsList/NewsList.tsx
@@ -19,6 +19,6 @@ const NewsList = ({news}: NewsListType) => {
   );
 };
 
-const NewsListWithSkeleton = withSkeleton(NewsList, 'item', 10)
+const NewsListWithSkeleton = withSkeleton(NewsList, 'item', 10, 'column')
 
 export default NewsListWithSkeleton;
